fd0df31fc8f2db193f2ac889fa869a5550e84523
jhashreya516 <77039610+jhashreya516@users.noreply.github.com>
2022-08-03 11:52:19 +0530

    Update Malaria_detection.py

--- Malaria_detection.py
+++ Malaria_detection.py
@@ -1,13 +1,5 @@
 
-# -*- coding: utf-8 -*-
-"""
-@author: Krish.Naik
-"""
 
-## MAlaria Detection using Transfer Learning
-
-# Download The Dataset from 
-#https://www.kaggle.com/iarunava/cell-images-for-detecting-malaria
 
 from keras.layers import Input, Lambda, Dense, Flatten
 from keras.models import Model
